51c5f63576fb183380afc0c11f759b548b6a6065
cambriajm <cambriajm@CAMDELL>
2026-02-11 13:20:39 -0700

    start

--- ConvertAndValidate/ConvertAndValidate/Program.cs
+++ ConvertAndValidate/ConvertAndValidate/Program.cs
@@ -4,5 +4,16 @@
 
 static void Main(string[] args)
 {
+    Console.WriteLine("Enter a number please!");
+    string userInput = "";
 
+    try
+    {
+        int result = int.Parse(userInput);
+        Console.WriteLine($"Great! {userInput} is a number!");
+    }
+    catch
+    {
+
+    }
 }
